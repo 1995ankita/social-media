7edfae90eb93d1677fb74f96f71c33c6474e21fe
1995ankita <ankita.hirpara@theknowledgeacademy.com>
2023-12-29 18:32:32 +0530

    first commits

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Sidebar from './components/sidebar'
+
+function App() {
+
+
+  return (
+    <>
+      <Header></Header>
+      <Sidebar></Sidebar>
+
+      <Footer></Footer>
+    </>
+
+  )
+
+}
+
+export default App
